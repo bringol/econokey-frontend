2ed1ca1ce3f4160fc4efaec7d8b1667c6db2b065
BoyerNicolas <nicolas.boyer@argontech.com.ar>
2022-05-18 19:53:52 -0300

    main screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Container } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainScreen from './Components/MainScreen';
 
 function App() {
+
+  const accountList = [
+    {id: 1, titulo: 'Gmail', descripcion: 'Gmail.com', type: 'pass'},
+    {id: 2, titulo: 'Binance', descripcion: 'Virtual Wallet', type: 'wallet'},
+    {id: 3, titulo: 'Notas', descripcion: 'No te olvides de tomar agua', type: 'note'}
+  ];
+
+  const [accounts, setAccounts] = React.useState(accountList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container fluid
+        sx={{
+          bgcolor: '#FBFDF7'
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainScreen accounts={accounts} />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </>
   );
 }
 
