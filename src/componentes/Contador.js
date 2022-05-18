cb293e1ee433a735bbaf4ca2a4e1bc63f6a8b9cc
Bruno <cingolanib@gmail.com>
2022-05-18 15:50:11 -0300

    Comienzo Passphrase
    Usar npm install para tener la nueva librería zxcvbn usada como indicador de contraseña

---
+++ src/componentes/Contador.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function Contador() {
+  const [num, setNum] = useState(0);
+
+  function sumar(){
+      setNum(num+1)
+  }
+  
+  function restar() {
+        if(num > 0)
+        setNum(oldCount => oldCount - 1)}
+
+  return (
+    <div className="app">
+      <h4>Palabras</h4>
+      <p>{num}</p>
+        <button onClick={restar}>-</button>
+        <button onClick={sumar}>+</button>
+      
+    </div>
+  );
+}
+
+export default Contador
