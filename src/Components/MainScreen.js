91e96530a9016c2756d32e8ca777caadc8f0f99a
BoyerNicolas <niboyer@uade.edu.ar>
2022-05-19 09:21:07 -0300

    MainScreen with add button and account cards

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -10,14 +10,37 @@ const MainScreen = ({ accounts }) => {
         <>
             {(accounts !== undefined) && (accounts.length > 0) ? (
                 <Box height={'100vh'} display={'flex'} flexDirection={'column'}>
-                    {
-                        accounts.map((account, index) => {
-                            return (
-                                <MainScreenCard account={account} index={index} />
-                            )
-                        })
-                    }
+                    <Box flex={1} overflow={'auto'} flexDirection={'column'}  display={'flex'} >                        {
+                            accounts.map((account, index) => {
+                                return (
+                                    <MainScreenCard account={account} index={index} />
+                                )
+                            })
+                        }
+                    </Box>
+                    <Box flex={1} overflow="auto" flexDirection={'row'} alignItems={'flex-end'} display={'flex'} justifyContent={'right'}>
+                        <Box
+                            sx={{
+                                margin: '20px',
+                                height: 56,
+                                width: 56,
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                backgroundColor: '#9CF6B1',
+                                boxShadow: '0px 0.25px 3px rgba(0, 0, 0, 0.039), 0px 2.75px 9px rgba(0, 0, 0, 0.19)',
+                                borderRadius: 4
+                            }}
+                        >
+                            <AddIcon
+                                sx={{
+                                    fontSize: 24
+                                }}
+                            />
+                        </Box>
+                    </Box>
                 </Box>
+
             ) : (
                 <Box height={'100vh'} display={'flex'} flexDirection={'column'}>
                     <Box flex={1} overflow={'auto'} flexDirection={'row'} alignItems={'flex-end'} display={'flex'} justifyContent={'center'}>
