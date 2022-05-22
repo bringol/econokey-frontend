14e020d220a364ca43e5bf07e00c25eade8a404c
Bruno <cingolanib@gmail.com>
2022-05-21 21:59:57 -0300

    Estilos modificados

--- src/Components/Passphrase.js
+++ src/Components/Passphrase.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import {Avatar,Button,CssBaseline,TextField,Input ,Container, Box,Grid} from '@mui/material';
+import {Avatar,Button,CssBaseline,TextField,Input ,Typography, Box,Grid} from '@mui/material';
 import { makeStyles } from '@mui/styles'; 
 import CasinoIcon from '@mui/icons-material/Casino';
 import Dropdown from './Dropdown';
 import Contador from './Contador';
 import Indicador from "./PasswordStrMeter"
-import {FaDiceD20} from 'react-icons/fa'
+import {FaDiceD20,FaPlusCircle,FaMinusCircle} from 'react-icons/fa'
+import {BsTrash} from "react-icons/bs"
+import {ImFloppyDisk} from "react-icons/im"
+
 
 
 //sacados del componente Dropdown, en el futuro encontrar manera de paramentrizar
@@ -26,22 +29,32 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: "1px",
-    backgroundColor: "#834e6d",
+    backgroundColor: "#E7F2E8",
   },
   form: {
-    width: '100%', 
-    marginTop: "1px",
+    //width: '100%', 
+    // marginTop: "1px",
+    textAlign: 'center',
+    //display: 'flex',
+    //flexDirection: 'column',
+    backgroundColor: "#E7F2E8",
+    //justifyContent: 'center',
+    borderRadius: '16px', borderColor: 'black',display: 'flex', justifyContent: 'center'
+    
   },
   submit: {
     margin: "8px",
   },
   botón: {
-    marginTop: "8px",
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginBottom: "100px",
-    backgroundColor: "#834e6d",
+    // marginTop: "8px",
+    // display: 'flex',
+    // flexDirection: 'column',
+    // alignItems: 'center',
+    // marginBottom: "100px",
+    // backgroundColor: "#834e6d",
+    size:"50px",
+
+    
   },
   centrado: {
     margin: '20px auto',
@@ -89,23 +102,35 @@ function restar() {
 
   
     return (
-      <Container component="main" maxWidth="xs">
-        <div className={classes.paper}>
-           <h2>Generar Passphrase </h2>
-          
-
-          <form className={classes.form} autoComplete="off" noValidate onSubmit={handleSubmit}>
-          <Grid container direction="column">
-            <Grid item xs={12}>
-                <Box mt={3}>
-                    <TextField 
-                    sx={{ m: 1, width: 140, flexDirection: 'column', textAlign: 'center' }}
-                    inputProps={{maxLength: 1,}}
+      <Grid container className={classes.form} style={{borderStyle:"solid"}}
+        // sx={{ borderRadius: '16px', borderColor: 'black',display: 'flex', justifyContent: 'center' }}
+         >
+           
+           <h2>Generar Passphrase</h2>
+          <form  autoComplete="off" noValidate onSubmit={handleSubmit}>
+            <Grid item xs={12} >
+                <Box 
+                // m={11}                
+                //width= "140"
+                // display="flex"
+                // flexDirection="row-reverse"
+                // alignItems= 'center'
+                //style={{borderStyle:"solid"}}
+                
+                sx={{
+                  mt: 3,
+                  paddingRight:15,   
+                  paddingLeft:15,                 
+                  marginBottom: "50px",               
+                }}
+                >
+                    <TextField  inputProps={{maxLength: 1,  style: {textAlign: 'center',fontSize: 40.0, height: 15.0}}}
                       
                       name="delimitador"
                       label="Delimitador"                    
                       color="secondary"
-                      size="small"                  
+                      size="large"
+                      //autoWidth                 
                       onChange={e => setDelim(e.target.value)}
                       />
                 </Box>     
@@ -113,8 +138,7 @@ function restar() {
 
             <Grid item xs={12}>                      
                 <Box mt={3}>
-                    {/* <Dropdown/> */}
-                    <div>
+                    
                       <FormControl sx={{ m: 1, minWidth: 130 }}>
                         <InputLabel id="dropdown">Capitalización</InputLabel>
                         <Select
@@ -123,7 +147,7 @@ function restar() {
                           value={cap}
                           //onChange={handleChange}
                           onChange={e => setCap(e.target.value)}
-                          autoWidth
+                          //autoWidth
                           label="Capitalización"
                         >
                           <MenuItem value="minúscula">
@@ -134,7 +158,7 @@ function restar() {
                           
                         </Select>
                       </FormControl>
-                    </div>
+                    
                 </Box>
             </Grid>            
 
@@ -142,78 +166,124 @@ function restar() {
 
             <Grid item xs={12}>                      
                 <Box mt={3}>
-                <div className="app">
-                  <h4>Palabras</h4>
-                  <p>{num}</p>
-                    <button onClick={restar}>-</button>
-                    <button onClick={sumar}>+</button>
+                <div>
+                  <h2>Palabras</h2>
+                  <Typography sx={{ fontSize: 60 }}>
+                    {num}
+                  </Typography>
+                    
+                    
+                    <Button
+                     onClick={restar}
+                     sx={{ fontSize: 30,color:"#0F1833" }}
+                     
+                     >
+                       <FaMinusCircle/>
+                     
+                     
+                     </Button>
+                    <Button
+                     onClick={sumar}
+                     sx={{ fontSize: 30,color:"#0F1833" }}
+                     >
+                       <FaPlusCircle/>
+                    </Button>
                 
                 </div>
                 </Box>
               </Grid>
 
 
+              <Grid item xs={12} >              
+                  <Typography sx={{ fontSize: 25,mt:8}}>
+                    Generar
+                  </Typography>
+                  <Button 
+                  type="submit"
+                  sx={{ fontSize: 100,color:"#0F1833"}}
+                  >
+                     <FaDiceD20 />
+                  </Button>
+                
+                </Grid>
 
-                <div>
-                  {/* <input name="generar"/> */}
-                  <button type="submit"><FaDiceD20 /></button>
-                </div>
-
-                <Grid item xs={12}>
-                <Box mt={3}>
+                <Grid item xs={12} >
+                <Box mt={3} m={3}>
             
                     <TextField 
                     id="contraseña"
                     label="Contraseña"
-                    //variant="standard"
-                    type="password"
+                    variant="standard"
+                    //type="password"
                     color="secondary"
-                    size="small"
+                    size="big"
+                    maxRows={12}
                     onChange={e => setPassword(e.target.value)}
-                    fullWidth
-                    required               
+                    fullWidth                          
                     />
                     <Indicador password={password} />
 
                 </Box>
             </Grid>
+            
+            <Grid item xs={12} >
+                <Box mt={3} m={3}>
+            
+                    <TextField 
+                    id="entropía"
+                    label="Entropía"
+                    variant="standard"
+                    //type="password"
+                    color="secondary"
+                    size="big"
+                    maxRows={12}
+                    //onChange={e => setPassword(e.target.value)}
+                    fullWidth                          
+                    />                   
+
+                </Box>
+            </Grid>
+            <Grid container>
+
             <Grid item xs={6}>
-                 <Box  mt={3} sx={{width: 150}}>
+                 <Box
+                  mt={3}
+                  // sx={{width: 100}}
+                  >
                      <Button
                      type="submit"
-                     fullWidth
+                     halfWidth
                      variant="contained"
-                     color="secondary"
-                     className={classes.botón}
+                     sx={{color:"#EB5757", backgroundColor: "#E7F2E8" ,borderRadius: '16px', paddingX:5}}
+                     
                      >
-                     Cancelar 
+                     Cancelar <BsTrash/> 
                      </Button>
                 </Box>
             </Grid>
             
                 <Grid item xs={6}>
-                <Box  mt={3} sx={{width: 150}}>
+                <Box  
+                mt={3}
+                
+                // sx={{width: 100}}
+                >
                     <Button
                             type="submit"
-                            fullWidth
+                            halfWidth
                             variant="contained"
-                            color="secondary"
-                            className={classes.botón}
+                            //color="secondary"
+                            //className={classes.botón}
+                            sx={{color:"#0F1833", backgroundColor: "#D3E8D3",borderRadius: '16px', paddingX:5}}
                             
                         >
-                    Usar 
+                    Utilizar <ImFloppyDisk />
                     </Button>
                 </Box>
                 </Grid>
-
+                </Grid>
+                </form>
             </Grid>
-                
-            
-            
-          </form>
-          
-        </div>
-      </Container>
 
     )
   
